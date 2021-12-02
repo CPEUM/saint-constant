4dea1c7a8e38ea7a182b4d1006df6b9564542116
Emmanuel Beaudry Marchand <emmbema@gmail.com>
2021-12-02 11:50:57 -0500

    Temp remove of redirect and load function to test remote build error

--- src/routes/ideation/__layout.svelte
+++ src/routes/ideation/__layout.svelte
@@ -1,21 +1,21 @@
 <script context="module">
-	export async function load({page}) {
-		let queriedExercice = exerciceRoutes.find(exercice => exercice.path === page.path);
-		if (queriedExercice) {
-			routes.setIdeation(page.path);
-			return {
-				props: {
-					exercice: exerciceRoutes.find(ex => ex.path === page.path)
-				}
-			}
-		}
-		else {
-			return {
-				status: 308,
-				redirect: exerciceRoutes[0].path
-			}
-		}
-	}
+	// export async function load({page}) {
+	// 	let queriedExercice = exerciceRoutes.find(exercice => exercice.path === page.path);
+	// 	if (queriedExercice) {
+	// 		routes.setIdeation(page.path);
+	// 		return {
+	// 			props: {
+	// 				exercice: exerciceRoutes.find(ex => ex.path === page.path)
+	// 			}
+	// 		}
+	// 	}
+	// 	else {
+	// 		return {
+	// 			status: 308,
+	// 			redirect: exerciceRoutes[0].path
+	// 		}
+	// 	}
+	// }
 </script>
 
 
