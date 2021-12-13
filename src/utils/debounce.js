ed4f56d321133f8a839b5dd22479c8246d06c904
Emmanuel Beaudry Marchand <emmbema@gmail.com>
2021-12-13 01:14:10 -0500

    Implement debounce helper

--- src/utils/debounce.js
+++ src/utils/debounce.js
@@ -1,6 +1,13 @@
 /**
  * Function to debounce a passed function's execution rate
  */
-export function debounce(task, interval = 250) {
-
+ export function debounce(f, delay = 250) {
+	let timer = null;
+	return (...args) => {
+		clearTimeout(timer);
+		timer = setTimeout(() => {
+			timer = null;
+			f(...args);
+		}, delay);
+	}
 }
