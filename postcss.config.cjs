acac1a3b09e7574a59eba13859c4b8bf8674a3a1
Emmanuel Beaudry Marchand <emmbema@gmail.com>
2022-02-22 22:42:34 -0500

    Add postcss filter log to see whadafucknotworkin

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -2,7 +2,10 @@ module.exports = {
 	plugins: [
 		require('postcss-url')({
 			filter: '/**',
-			url: (asset) => `${process.env.PUBLIC_BASE_PATH || ''}${asset.url}`
+			url: (asset) => {
+				console.log(asset.url + ' to --> ' + `${process.env.PUBLIC_BASE_PATH || ''}${asset.url}`);
+				return `${process.env.PUBLIC_BASE_PATH || ''}${asset.url}`;
+			}
 		}),
 		require('postcss-nested'),
 		require('@daltontan/postcss-import-json'),
